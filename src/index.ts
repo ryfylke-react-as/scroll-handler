dce45c61e3edc690ebf42de7d834b26b6483a67d
Håkon <haakonu97@gmail.com>
2023-07-23 18:01:13 +0200

    fix: getPercent

--- src/index.ts
+++ src/index.ts
@@ -112,7 +112,9 @@ export class ScrollHandler {
       (event, { disable }) =>
         effect(event, {
           getPercent: () => {
-            return ((this.scrollTop + after) / before) * 100;
+            const totalDistance = before - after;
+            const scrolledDistance = this.scrollTop - after;
+            return (scrolledDistance / totalDistance) * 100;
           },
           disable,
         })
